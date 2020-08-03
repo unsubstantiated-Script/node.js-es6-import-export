57b9a2b5f844b5b8006203111c3c85e624861419
Justin Fulton <jusfulton@gmail.com>
2020-08-03 12:30:16 -0700

    working now!

--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,4 @@
-export function rect(x, y, callback) {
+function rect(x, y, callback) {
   if (x <= 0 || y <= 0) {
     callback(
       new Error(
@@ -8,11 +8,15 @@ export function rect(x, y, callback) {
   } else {
     setTimeout(
       () =>
-        callback(null, {
-          perimeter: () => 2 * (x + y),
-          area: () => x * y,
-        }),
+      callback(null, {
+        perimeter: () => 2 * (x + y),
+        area: () => x * y,
+      }),
       2000
     );
   }
+}
+
+export {
+  rect
 }
